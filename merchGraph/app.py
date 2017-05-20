2ac1b67476378b1b8b955d9c331e214753291d54
Sarthak Khanna <khannasarthak.1994@gmail.com>
2017-05-20 02:31:50 -0700

    Added basic functionality
    Added database of mvp's and single mvp working right now.

--- merchGraph/app.py
+++ merchGraph/app.py
@@ -1,32 +1,47 @@
-# from flask import Flask, render_template,request
-# from flask_mysqldb import MySQL
-#
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def index():
-#     return ('index.html')
-#
-#
-#
-# if __name__=='__main__':
-#     app.run(debug = True)
-
 from flask import Flask
 from flask import Markup
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from datetime import datetime,timedelta
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/tromvp'
+db = SQLAlchemy(app)
+
+
+class mvp(db.Model):
+	__tablename__ = 'mvp'
+	name = db.Column('name',db.Unicode, primary_key=True)
+	lowertime = db.Column('lowertime', db.Integer)
+	uppertime = db.Column('uppertime', db.Integer)
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/mvpdb")
-def mvpdb():
-    return render_template('mvpdb.html')
+@app.route("/mvpdb",methods = ['POST', 'GET'])
+def mvpdb():	
+	return render_template('mvpdb.html')
+
+@app.route("/mvpcheck",methods = ['POST', 'GET'])
+def mvpcheck():	
+	formname = request.form['mvpname']
+	now = datetime.now()
+	dietime = now.strftime("%H:%M")
+	mvpdetails = mvp.query.filter(mvp.name==formname)
+	# print ('/////',mvpdetails)
+	for details in mvpdetails:
+		lowtime = details.lowertime
+		uptime = details.uppertime
+	# print ("-------",lowtime,'==============',uptime)
+	reslow =  (now + timedelta(minutes=int(lowtime))).strftime("%H:%M")
+	resup  =  (now + timedelta(minutes=int(uptime))).strftime("%H:%M")
+	print ('++++', reslow,'+++',resup)
+
+	return render_template('mvpdb.html', mvpdetails = mvpdetails, dietime = dietime, reslow = reslow, resup= resup)
 
 
 @app.route("/chart")
--- merchGraph/static/the-basics.js
+++ merchGraph/static/the-basics.js
@@ -20,15 +20,18 @@ var substringMatcher = function (strs) {
     };
 };
 
-var states = ['Phreeoni (moc_fild17)', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+var states = ['Amon Ra (moc_pryd06)','Angeling (pay_fild04)','Angeling (xmas_dun01)','Angeling (yuno_fild03)','Archangeling (yuno_fild05)',
+'Atroce (ra_fild02)','Atroce (ra_fild03)','Atroce (ra_fild04)','Atroce (ve_fild01)','Atroce (ve_fild02)','Bacsojin (lou_dun03)',
+'Baphomet (prt_maze03)','Beelzebub (abbey03)','Boitata (bra_dun02)','Dark Lord (gld_dun04)','Dark Lord (gl_chyard)','Detale (abyss_03)',
+'Deviling (pay_fild04)','Deviling (yuno_fild03)','Doppelganger (gef_dun02)','Dracula (gef_dun01)','Drake (treasure02)','Eddga (pay_fild11)',
+'Egnigem Cenia (lhz_dun02)','Evil Snake Lord (gon_dun03)','Fallen Bishop Hibram (abbey02)','Garm (xmas_fild01)','Ghostring (pay_fild04)',
+'Ghostring (prt_maze03)','Ghostring (treasure02)','Gloom Under Night (ra_san05)','Golden Thief Bug (prt_sewb4)','Gopinich (mosk_dun03)',
+'Hardrock Mammoth (man_fild03)','Ifrit (thor_v03)','Incantation Samurai (ama_dun03)','Kiel D-01 (kh_dun02)','Kraken (iz_dun05)',
+'Ktullanux (ice_dun03)','Lady Tanee (ayo_dun02)','Leak (dew_dun01)','Lord of Death (niflheim)','Maya (anthell02)','Maya Purple (anthell01)',
+'Mistress (mjolnir_04)','Moonlight Flower (pay_dun04)','Orc Hero (gef_fild02)','Orc Hero (gef_fild14)','Orc Lord (gef_fild10)',
+'Osiris (moc_pryd04)','Pharaoh (in_sphinx5)','Phreeoni (moc_fild17)','Queen Scaraba (dic_dun02)','RSX-0806 (ein_dun02)','Stormy Knight (xmas_dun02)',
+'Tao Gunka (beach_dun)','Tao Gunka (beach_dun2)','Tao Gunka (beach_dun3)','Tendrillion (spl_fild03)','Thanatos (thana_boss)',
+'Turtle General (tur_dun04)','Valkyrie Randgris (odin_tem03)','Vesper (jupe_core)','Wounded Morroc (moc_fild22)'
 ];
 
 $('#the-basics .typeahead').typeahead({
