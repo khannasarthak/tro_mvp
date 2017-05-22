c5e01c10e896163c8b3719a2ea1b7d57f32e8000
Sarthak Khanna <khannasarthak.1994@gmail.com>
2017-05-21 21:45:22 -0700

    New stuff added
    Might need to revert back to this later.

--- merchGraph/app.py
+++ merchGraph/app.py
@@ -25,6 +25,14 @@ class deadmvp(db.Model):   #dead mvp table
 	def __init__(self,name, deathtime):	# needed to insert/update
 		self.name = name
 		self.deathtime = deathtime
+
+class users(db.Model):   #dead mvp table
+	__tablename__ = 'users'
+	name = db.Column('username',db.Unicode, primary_key=True)
+
+	def __init__(self,username):	# needed to insert/update
+		self.username = username
+		
 	
 
 
@@ -34,33 +42,51 @@ def index():
 
 @app.route("/mvpdb",methods = ['POST', 'GET'])
 def mvpdb():	
-	test = deadmvp('Eddga (pay_fild11)','04:06')
-	print ('++++++++++', test)
-	db.session.add(test)
-	db.session.commit()
 	return render_template('mvpdb.html')
 
 @app.route("/mvpcheck",methods = ['POST', 'GET'])
 def mvpcheck():	
 	formname = request.form['mvpname']
+	mvpname = deadmvp.query.filter(deadmvp.name==formname).first()
 	now = datetime.now()
 	dietime = now.strftime("%H:%M")
+	if mvpname is not None:		#Update if exists
+		# print ('EXSITS')
+		update_this = deadmvp.query.filter_by(name = formname).first()		
+		# print (update_this)
+		update_this.deathtime = dietime
+		# print ('AFTER UPDATE',update_this)
+		db.session.commit()
+	else:	# Insert if doesnt exist
+		# print ('DOESNT EXSITS')
+		row = deadmvp(formname,dietime)
+		db.session.add(row) 	# to update
+		db.session.commit()
+		 
+		
+
+	
 	mvpdetails = mvp.query.filter(mvp.name==formname)
+	deadmvpdetails = deadmvp.query.filter(mvp.name==formname)
 	
 	for details in mvpdetails:
 		lowtime = details.lowertime
 		uptime = details.uppertime
-	# print ("-------",lowtime,'==============',uptime)
+
+	
 	reslow =  (now + timedelta(minutes=int(lowtime))).strftime("%H:%M")
 	resup  =  (now + timedelta(minutes=int(uptime))).strftime("%H:%M")
-	# print ('++++', reslow,'+++',resup)
+	
 
-	test = deadmvp(formname,dietime)
-	print ('++++++++++', test)
-	db.session.add(test)
-	db.session.commit()
+	return render_template('mvpdb.html', mvpdetails = mvpdetails, dietime = dietime, reslow = reslow, resup= resup, deadmvpdetails = deadmvpdetails)
 
-	return render_template('mvpdb.html', mvpdetails = mvpdetails, dietime = dietime, reslow = reslow, resup= resup)
+
+@app.route("/login")
+def login():    
+    return render_template('login.html')
+
+
+
 
 
 @app.route("/chart")
@@ -68,6 +94,9 @@ def chart():
     labels = ["January","February","March","April","May","June","July","August"]
     values = [10,9,8,7,6,4,7,8]
     return render_template('chart.html', values=values, labels=labels)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
